d5c074ac14b01f3f32c3d3117782a3b700fc93ea
Enayat7800 <153251094+Enayat7800@users.noreply.github.com>
2023-12-17 14:08:36 +0530

    Update info.py

--- info.py
+++ info.py
@@ -2,7 +2,7 @@ API_ID       = 28150346
 API_HASH     = "426f0d0a1da02dea8fb71cb0bd3ab7e1"
 BOT_TOKEN    = "6078348705:AAFQgkwjl825xkOJbPEH-QkaK5v8GufJXJk"
 SESSION      = "1BVtsOK0Bu1I72v-WyQ90YzRBFVZQ32dYeILX3E6U3YfR-xZzMIZFb94Kc1BA4GUFQV6LtajF1ITOAHoztb8sS0kqKJKf4eL5uq53E7db_yh3d1_dEof-oqN-7DHl7E0jUoWUa2Lcz6yuFe7DBSTI9gIE_0fTxFhC4wK9XTH3LvLeJVeG-oRY5Uuh0426BmGgQptqdQWW3SFyoUNjs1yQ3KkCJSVnsr61jfMEjITcV1faYWcryE4KTY6W9x0UD_soHgnIGbDzYb4iXZuhGzGcjufddhxz1TZGLyz3EeZ4dW8UpVF-ex60VqW_eLnIe4SldSBT2C0ilCQ2T_Yjjos7aF_zyPzMwZg="
-DATABASE_URI = "mongodb+srv://inayatalibarkaat:inayatali@cluster0.2iwzvoh.mongodb.net/?retryWrites=true&w=majority"
+DATABASE_URI = "mongodb+srv://inayatalibarkaat:enayatalibarkaat@cluster0.1gt1a4m.mongodb.net/?retryWrites=true&w=majority"
 LOG_CHANNEL  = -1001916792320
 ADMIN        = 1251962299
 CHANNEL      = "@moviesworldd7800"
